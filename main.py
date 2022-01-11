4a3fe6d8d6f1b141d67af1c99565060ffb324e2e
Filipe Andrade <filipe.engenharia42@gmail.com>
2022-01-11 20:31:25 -0300

    descoberta working on python

---
+++ main.py
@@ -0,0 +1,71 @@
+import asyncio
+import os
+import signal
+import json
+from nats.aio.client import Client as NATS
+
+
+async def main():
+    nc = NATS()
+
+    async def closed_cb():
+        print("Connection to NATS is closed.")
+        await asyncio.sleep(0.1)
+        asyncio.get_running_loop().stop()
+
+    # It is very likely that the demo server will see traffic from clients other than yours.
+    # To avoid this, start your own locally and modify the example to use it.
+    options = {
+        # "servers": ["nats://127.0.0.1:4222"],
+        "servers": ["nats://localhost:4222"],
+        "closed_cb": closed_cb
+    }
+
+    await nc.connect(**options)
+    print(f"Connected to NATS at {nc.connected_url.netloc}...")
+
+    async def subscribe_handler(msg):
+        subject = msg.subject
+        reply = msg.reply
+        data = msg.data.decode()
+        print("Received a message on '{subject} {reply}': {data}".format(
+            subject=subject, reply=reply, data=data))
+        await msg.respond(b'I can help!')
+
+    # Basic subscription to receive all published messages
+    # which are being sent to a single topic 'discover'
+    await nc.subscribe("help", cb=subscribe_handler)
+
+    # --------------
+
+    async def servico_handler(msg): # implementar com os metodos
+        print(msg.data.decode())
+
+    sv_id = await nc.request("descoberta", json.dumps({"endereco_ip": "192.168.1.10", "porta": 1234, "armazenamento": "/home/filipe/Documents/1", "horas_retencao": 1}).encode("utf-8"), timeout=1)
+    await nc.subscribe(str(sv_id)+".>", cb=servico_handler)
+
+    # --------------
+
+    # # Subscription on queue named 'workers' so that
+    # # one subscriber handles message a request at a time.
+    # await nc.subscribe("help.*", "workers", subscribe_handler)
+
+    def signal_handler():
+        if nc.is_closed:
+            return
+        print("Disconnecting...")
+        asyncio.create_task(nc.close())
+
+    for sig in ('SIGINT', 'SIGTERM'):
+        asyncio.get_running_loop().add_signal_handler(getattr(signal, sig), signal_handler)
+
+    await nc.request("help", b'help')
+
+    await asyncio.sleep(10000)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print(e)
+        pass
